7308a0a07874db6306cafa57edf6497e4b40c5d1
Alex <mapimopipi@gmail.com>
2014-06-13 09:01:21 +0400

    remove migration

--- db/migrate/20140529032749_create_listings_locations_joins_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateListingsLocationsJoinsTable < ActiveRecord::Migration
-  def change
-    create_table  :listings_locations_joins_table do |t|
-    	t.integer :listing_id
-    	t.integer :location_id    	
-    end
-  end
-end
