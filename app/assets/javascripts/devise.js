5ad5256a51ea6aa8f44e53b6a8a1502185ac6def
mapimopi <mapimopipi@gmail.com>
2014-06-16 10:35:30 +0400

    divide name into two fields

---
+++ app/assets/javascripts/devise.js
@@ -0,0 +1,26 @@
+function ready() {
+  if($("input#user_name").val() != '') {
+    full_name = $("input#user_name").val().split(' ');
+    $("#first_name").val(full_name.slice(0, -1));
+    $("#last_name").val(full_name.pop());
+  }
+
+  $("form#new_user").on("submit", function() {
+    first_name = $("#first_name");
+    last_name = $("#last_name");
+    if (first_name.val() == '') {
+      first_name.focus();
+      first_name.parent().addClass("field_with_errors");
+      return false;
+    }
+    if (last_name.val() == '') {
+      last_name.focus();
+      last_name.parent().addClass("field_with_errors");
+      return false;
+    }
+    $("input#user_name").val(first_name.val() + ' ' + last_name.val());
+  });
+}
+
+$(document).ready(ready);
+$(document).on("page:load", ready);
