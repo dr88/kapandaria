ae48260488c6b69b360aaa53a8c40e581abf616f
mapimopi <mapimopipi@gmail.com>
2014-06-18 12:34:37 +0400

    expandable navbar

--- app/assets/javascripts/listings_.js
+++ app/assets/javascripts/listings_.js
@@ -1,7 +1,4 @@
 function listingsReady() {
-	// Dynamic search of listings
-	$("section.sieve").sieve({ itemSelector: "div.col-md-3" });	
-	
 	// Change background of listings when hovering
 	$('.thumbnail').hover(
 		function() {
@@ -10,7 +7,7 @@ function listingsReady() {
 	            $(this).css('background-color', '#FFFFFF');}
 	);
 
-	el = $("#form-field-tags");
+	var el = $("#form-field-tags");
 	if (el.length) {
 		el.tag({
 			placeholder: "Enter new locations ...",
@@ -25,12 +22,48 @@ function listingsReady() {
 	});
 
 	// Moving the search box
-	el = $('form[role=\'search\']')[0];
+	el = $('div[role=\'search\']')[0];
 	if (el) {
-		$('form[role=\'search\']')[0].remove();
-			$(el).appendTo('.navbar-search');  
+		$('div[role=\'search\']')[0].remove();
+		$(el).appendTo('.navbar-search');  
+	}
+	// Moving the advanced search tools
+	el = $('div[role=\'advanced-search\']')[0];
+	if (el) {
+		$('div[role=\'advanced-search\']')[0].remove();
+		$(el).appendTo('.navbar-advanced-search');  
 	}
 
+	$("#show-advanced-search").on("click", function () {
+		var icon = this.children[0]
+		if (icon.getAttribute("class") == "glyphicon glyphicon-chevron-down") {
+			// expand
+			$(".navbar-default").css("max-height", '406px');
+			icon.setAttribute("class", "glyphicon glyphicon-chevron-up")
+		} else {
+			// shrink
+			$(".navbar-default").css("max-height", '52px');
+			icon.setAttribute("class", "glyphicon glyphicon-chevron-down")
+		}
+	});
+
+
+	// Search
+	var query;
+	function doSearch () {
+		console.log("search");
+	}
+
+	var timer;
+	function delaySearch () {
+		timer = setTimeout(doSearch, 1200);
+	}
+
+	$("#search-box").on("input", delaySearch);
+	$(".tags input[type='text']").on("input", delaySearch);
+	$(".tags input[type='text']").on("blur", doSearch);
+	$(document).on("click", ".tag .close", doSearch);
+	$("#women_only").on("change", doSearch);
 };
 
 $(document).ready(listingsReady);
