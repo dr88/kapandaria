463626e3cfa6db9b4debc20a4fb1539a90f19b4d
mapimopi <mapimopipi@gmail.com>
2014-06-25 06:57:52 +0400

    search goes through whole word

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -25,7 +25,7 @@ class Listing < ActiveRecord::Base
 		unless query.empty?
 			listings = all
 			listings = listings.where(women_only: false) unless query.has_key? :women_only
-			listings = listings.where("LOWER(listings.name) LIKE :name", name: "#{query[:search].downcase}%") unless query[:search].blank?
+			listings = listings.where("LOWER(listings.name) LIKE :name", name: "%#{query[:search].downcase}%") unless query[:search].blank?
 			listings = listings.joins(:locations).where("LOWER(locations.name) IN (:tags)", tags: query[:tags].downcase.split(',')) unless query[:tags].blank?
 			listings.uniq
 		else
